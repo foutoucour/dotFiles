bf8ee8e49ebdc326dc724299bb20e21d64ac3ad6
Jordi Riera <jriera@rodeofx.com>
2017-04-28 10:16:26 -0400

    improvement of bash environments

--- envFiles/rdo_env.bash
+++ envFiles/rdo_env.bash
@@ -3,8 +3,8 @@ if [ ! -z "$PS1" ]; then
     echo -e "\033[0;32mSetting custom aliases:\033[0m"
     echo -e " * aliases to folders: (l.{} to local, p.{} to prod)"
     echo -e "   - x.pylibs"
-    alias l.pylibs='cd ~/rdoenv/python_libs/pure/rodeo'
-    alias p.pylibs='cd ~/prod/python_libs/pure/rodeo'
+    alias l.pylibs='cd ~/rdoenv/python_libs/pure'
+    alias p.pylibs='cd /rodeo/setup/lib/python/pure'
     echo -e "   - x.workgroup"
     alias l.workgroups='cd ~/rdoenv/workgroups'
     alias p.workgroups='cd ~/prod/workgroups'
@@ -22,3 +22,18 @@ if [ ! -z "$PS1" ]; then
     alias ingest_bid='/rodeo/setup/bin/shotgun/ingestBid.py'
     alias admin_film_prod='cd /mntx/admin/02_FILM/1-IN\ PRODUCTION'
 fi
+
+# Darwin needs a bit more of attention to have a proper environment
+if [ $(uname) == "Darwin" ]; then
+    export RDO_PYTHONPATH=$HOME/rdoenv/python_libs/pure:/rodeo/repositories/_libs/bin/linters/lib/site-packages:/rdo/rodeo/workgroups/maya/scripts:/rodeo/setup/lib/python/pure:/software/alembic/linux/current/lib:/software/qube/osx/current/api/python:$PYTHONPATH
+fi
+
+echo "Sourcing rez"
+
+export PATH=$PATH:/Users/jriera/rez/bin/rez
+. /Users/jriera/rez/completion/complete.sh
+export REZ_CONFIG_FILE=/rdo/rodeo/setup/rez/rdo_rez_config/rdo_rez_config.py
+
+echo "PythonZ"
+[[ -s $HOME/.pythonz/etc/bashrc ]] && source $HOME/.pythonz/etc/bashrc
+
